1b423e16b4dd0ddee4a4575021fbe98cac905754
lupingsu236 <72119175+lupingsu236@users.noreply.github.com>
2021-01-29 11:13:48 +0800

    added placeholder RouteFragment for SearchRouteActivity and SavedRoutesActivity; added map placeholder to MapActivity

--- app/src/main/java/com/example/mrt4you_mobile/SavedRoutesActivity.java
+++ app/src/main/java/com/example/mrt4you_mobile/SavedRoutesActivity.java
@@ -1,14 +1,34 @@
 package com.example.mrt4you_mobile;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Spinner;
 
-public class SavedRoutesActivity extends BaseActivity {
+public class SavedRoutesActivity extends BaseActivity implements AdapterView.OnItemSelectedListener {
+    Spinner mRoutesList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //populate list of saved routes in dropdown
+        mRoutesList = findViewById(R.id.routes_list);
+        //temp list of saved routes for the drown down
+        String[] routes = new String[]{"Home -> School", "Home -> Grandma", "Home -> Bugis"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_spinner_dropdown_item, routes);
+        mRoutesList.setAdapter(adapter);
+        mRoutesList.setOnItemSelectedListener(this);
+
     }
 
     @Override
@@ -19,5 +39,26 @@ public class SavedRoutesActivity extends BaseActivity {
     @Override
     int getNavigationMenuItemId() {
         return R.id.action_saved;
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String selected_route = (String) parent.getItemAtPosition(position);
+        replaceRouteFragment(selected_route);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {}
+
+    public void replaceRouteFragment(String selected_route) {
+        Bundle arguments = new Bundle();
+        arguments.putString("route", selected_route);
+        RouteFragment fragment = new RouteFragment();
+        fragment.setArguments(arguments);
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction trans = fm.beginTransaction();
+        trans.replace(R.id.fragment_route_container, fragment);
+        trans.addToBackStack(null);
+        trans.commit();
     }
 }
