fa86ff751baf70826732993f8b4aaed50df5ce67
liauhyj <72335756+liauhyj@users.noreply.github.com>
2021-02-09 16:58:47 +0800

    Logic for view news done. Pending connect to web API

--- app/src/main/java/com/example/mrt4you_mobile/NewsActivity.java
+++ app/src/main/java/com/example/mrt4you_mobile/NewsActivity.java
@@ -5,10 +5,16 @@ import androidx.core.app.NotificationCompatSideChannelService;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class NewsActivity extends BaseActivity
 {
@@ -39,30 +45,82 @@ public class NewsActivity extends BaseActivity
         return R.id.action_news;
     }
 
-    public static void updateView() throws JSONException
+    public void updateView() throws JSONException
     {
-        String data = "[{\"stationCode\":\"NS02\",\"stationName\":\"Bukit Batok\"," +
-                "\"timeToNextStation\":2147482647,\"timeToNextStationOpp\":2,\"currentStatus\":2," +
-                "\"time\":\"9/2/2021 1:56:29 PM\"}," +
-                "{\"stationCode\":\"NS03\",\"stationName\":\"Bukit Gombak\",\"timeToNextStation\"" +
-                ":15,\"timeToNextStationOpp\":15,\"currentStatus\":4, \"time\":" +
-                "\"10/1/2021 10:00:23 AM\"}," +
-                "{\"stationCode\":\"NS01\",\"stationName\":\"Jurong East\",\"timeToNextStation\"" +
-                ":0,\"timeToNextStationOpp\":10,\"currentStatus\":6," +
-                "\"time\":\"2/1/2021 11:53:02 PM\"}]";
-        JSONArray jsonArray = new JSONArray(data);
-        for (int i = 0; i < jsonArray.length(); i++)
+        new Thread(() ->
         {
-            JSONObject jsonObject = jsonArray.getJSONObject(i);
-            System.out.println(jsonObject);
-        }
+            List<TextView> newsList = new ArrayList<>();
+            List<TextView> timeList = new ArrayList<>();
+
+            TextView textNews1 = findViewById(R.id.textNews1);
+            newsList.add(textNews1);
+            TextView textTime1 = findViewById(R.id.textTime1);
+            timeList.add(textTime1);
+            TextView textNews2 = findViewById(R.id.textNews2);
+            newsList.add(textNews2);
+            TextView textTime2 = findViewById(R.id.textTime2);
+            timeList.add(textTime2);
+            TextView textNews3 = findViewById(R.id.textNews3);
+            newsList.add(textNews3);
+            TextView textTime3 = findViewById(R.id.textTime3);
+            timeList.add(textTime3);
+
+            String data = "[{\"stationCode\":\"NS02\",\"stationName\":\"Bukit Batok\"," +
+                    "\"timeToNextStation\":2147482647,\"timeToNextStationOpp\":2,\"currentStatus\":2," +
+                    "\"time\":\"9/2/2021 1:56:29 PM\"}," +
+                    "{\"stationCode\":\"NS03\",\"stationName\":\"Bukit Gombak\",\"timeToNextStation\"" +
+                    ":15,\"timeToNextStationOpp\":15,\"currentStatus\":4, \"time\":" +
+                    "\"10/1/2021 10:00:23 AM\"}," +
+                    "{\"stationCode\":\"NS01\",\"stationName\":\"Jurong East\",\"timeToNextStation\"" +
+                    ":0,\"timeToNextStationOpp\":10,\"currentStatus\":6," +
+                    "\"time\":\"2/1/2021 11:53:02 PM\"}]";
+            try
+            {
+                JSONArray jsonArray = new JSONArray(data);
+                for (int i = 0; i < jsonArray.length(); i++)
+                {
+                    JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+                    String time = jsonObject.get("time").toString();
+                    int finalI = i;
+                    runOnUiThread(() ->
+                    {
+                        timeList.get(finalI).setText(time);
+                    });
+
+                    String stationCode = jsonObject.get("stationCode").toString();
+                    String stationName = jsonObject.get("stationName").toString();
+                    int timeToNextStation = Integer.parseInt(jsonObject.get("timeToNextStation")
+                            .toString());
+                    int timeToNextStationOpp = Integer.parseInt(jsonObject
+                            .get("timeToNextStationOpp").toString());
+                    int currentStatus = Integer.parseInt(jsonObject.get("currentStatus").toString());
+                    runOnUiThread(() ->
+                    {
+                        newsList.get(finalI).setText(generateNewsMsg(stationCode, stationName,
+                                timeToNextStation, timeToNextStationOpp, currentStatus));
+                    });
+                }
+            }
+            catch (JSONException e)
+            {
+                e.printStackTrace();
+            }
+        }).start();
     }
 
-    public String generateNewsMsg (String stationCode, String stationName, int timeToNextStation, int timeToNextStationOpp, int currentStatus, String time)
+    public static String generateNewsMsg (String stationCode, String stationName,
+                                          int timeToNextStation, int timeToNextStationOpp,
+                                          int currentStatus)
     {
         String output = "";
         final String OPERATIONALSUFFIX = "is now fully operational.";
         final String BREAKDOWNBOTHSUFFIX = "is broken down in both directions";
+        final String DELAYEDBOTHSUFFIX = "is delayed in both directions. Travel time to the next" +
+                " stations in the direction of";
+        final String BREAKDOWNONEWAYSUFFIX = "is broken down in the direction of";
+        final String DELAYEDONEWAYSUFFIX = "is delayed in the direction of";
+        final String ONEWAYSUFFIX = "Travel time to the next station towards";
         final String NSPREFIX = "ns";
         final String EWPREFIX = "ew";
         final String CCPREFIX = "cc";
@@ -74,23 +132,109 @@ public class NewsActivity extends BaseActivity
             switch (currentStatus)
             {
                 case 1:
-                    output = String.format("%s %s %s", stationCode, stationName, BREAKDOWNBOTHSUFFIX);
+                    output = String.format("%s %s %s", stationCode, stationName,
+                            BREAKDOWNBOTHSUFFIX);
                     break;
                 case 2:
                     if (stationCode.toLowerCase().startsWith(NSPREFIX))
                     {
-                        output = String.format("%s %s is broken down in the direction of %s. Travel time in the direction of %s is %s.");
+                        output = String.format("%s %s %s %s. %s %s is %s.", stationCode,
+                                stationName, BREAKDOWNONEWAYSUFFIX, NSLINEFWDSTATION, ONEWAYSUFFIX,
+                                NSLINEOPPSTATION, timeToNextStationOpp);
                     }
                     else if (stationCode.toLowerCase().startsWith(EWPREFIX))
                     {
-
+                        output = String.format("%s %s %s %s. %s %s is %s.", stationCode,
+                                stationName, BREAKDOWNONEWAYSUFFIX, EWLINEFWDSTATION, ONEWAYSUFFIX,
+                                EWLINEOPPSTATION, timeToNextStationOpp);
                     }
                     else if (stationCode.toLowerCase().startsWith(CCPREFIX))
                     {
-
+                        output = String.format("%s %s %s %s. %s %s is %s.", stationCode,
+                                stationName, BREAKDOWNONEWAYSUFFIX, CCLINEFWDSTATION, ONEWAYSUFFIX,
+                                CCLINEOPPSTATION, timeToNextStationOpp);
                     }
                     break;
-
+                case 3:
+                    if (stationCode.toLowerCase().startsWith(NSPREFIX))
+                    {
+                        output = String.format("%s %s %s %s. %s %s is %s.", stationCode,
+                                stationName, BREAKDOWNONEWAYSUFFIX, NSLINEOPPSTATION, ONEWAYSUFFIX,
+                                NSLINEFWDSTATION, timeToNextStation);
+                    }
+                    else if (stationCode.toLowerCase().startsWith(EWPREFIX))
+                    {
+                        output = String.format("%s %s %s %s. %s %s is %s.", stationCode,
+                                stationName, BREAKDOWNONEWAYSUFFIX, EWLINEOPPSTATION, ONEWAYSUFFIX,
+                                EWLINEFWDSTATION, timeToNextStation);
+                    }
+                    else if (stationCode.toLowerCase().startsWith(CCPREFIX))
+                    {
+                        output = String.format("%s %s %s %s. %s %s is %s.", stationCode,
+                                stationName, BREAKDOWNONEWAYSUFFIX, CCLINEOPPSTATION, ONEWAYSUFFIX,
+                                CCLINEFWDSTATION, timeToNextStation);
+                    }
+                    break;
+                case 4:
+                    if (stationCode.toLowerCase().startsWith(NSPREFIX))
+                    {
+                        output = String.format("%s %s %s %s and %s are %s and %s, respectively.",
+                                stationCode, stationName, DELAYEDBOTHSUFFIX, NSLINEFWDSTATION,
+                                NSLINEOPPSTATION, timeToNextStation, timeToNextStationOpp);
+                    }
+                    else if (stationCode.toLowerCase().startsWith(EWPREFIX))
+                    {
+;                       output = String.format("%s %s %s %s and %s are %s and %s, respectively.",
+                                stationCode, stationName, DELAYEDBOTHSUFFIX, EWLINEFWDSTATION,
+                                EWLINEOPPSTATION, timeToNextStation, timeToNextStationOpp);
+                    }
+                    else if (stationCode.toLowerCase().startsWith(CCPREFIX))
+                    {
+                        output = String.format("%s %s %s %s and %s are %s and %s, respectively.",
+                                stationCode, stationName, DELAYEDBOTHSUFFIX, CCLINEFWDSTATION,
+                                CCLINEOPPSTATION, timeToNextStation, timeToNextStationOpp);
+                    }
+                    break;
+                case 5:
+                    if (stationCode.toLowerCase().startsWith(NSPREFIX))
+                    {
+                       output = String.format("%s %s %s %s. %s %s is now %s.", stationCode,
+                               stationName, DELAYEDONEWAYSUFFIX, NSLINEFWDSTATION, ONEWAYSUFFIX,
+                               NSLINEFWDSTATION, timeToNextStation);
+                    }
+                    else if (stationCode.toLowerCase().startsWith(EWPREFIX))
+                    {
+                        output = String.format("%s %s %s %s. %s %s is now %s.", stationCode,
+                                stationName, DELAYEDONEWAYSUFFIX, EWLINEFWDSTATION, ONEWAYSUFFIX,
+                                EWLINEFWDSTATION, timeToNextStation);
+                    }
+                    else if (stationCode.toLowerCase().startsWith(CCPREFIX))
+                    {
+                        output = String.format("%s %s %s %s. %s %s is now %s.", stationCode,
+                                stationName, DELAYEDONEWAYSUFFIX, CCLINEFWDSTATION, ONEWAYSUFFIX,
+                                CCLINEFWDSTATION, timeToNextStation);
+                    }
+                    break;
+                case 6:
+                    if (stationCode.toLowerCase().startsWith(NSPREFIX))
+                    {
+                        output = String.format("%s %s %s %s. %s %s is now %s.", stationCode,
+                                stationName, DELAYEDONEWAYSUFFIX, NSLINEOPPSTATION, ONEWAYSUFFIX,
+                                NSLINEOPPSTATION, timeToNextStationOpp);
+                    }
+                    else if (stationCode.toLowerCase().startsWith(EWPREFIX))
+                    {
+                        output = String.format("%s %s %s %s. %s %s is now %s.", stationCode,
+                                stationName, DELAYEDONEWAYSUFFIX, EWLINEOPPSTATION, ONEWAYSUFFIX,
+                                EWLINEOPPSTATION, timeToNextStationOpp);
+                    }
+                    else if (stationCode.toLowerCase().startsWith(CCPREFIX))
+                    {
+                        output = String.format("%s %s %s %s. %s %s is now %s.", stationCode,
+                                stationName, DELAYEDONEWAYSUFFIX, CCLINEOPPSTATION, ONEWAYSUFFIX,
+                                CCLINEOPPSTATION, timeToNextStationOpp);
+                    }
+                    break;
             }
         }
 
