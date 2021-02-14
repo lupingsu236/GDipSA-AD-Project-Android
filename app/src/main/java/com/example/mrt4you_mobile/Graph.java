3412184f342f8fc6cb966c381ee78c5eabf8c87e
liauhyj <72335756+liauhyj@users.noreply.github.com>
2021-02-14 16:39:55 +0800

    Removed train image from activity_start.xml, removed unnecessary imports in Graph.java and StartActivity.java

--- app/src/main/java/com/example/mrt4you_mobile/Graph.java
+++ app/src/main/java/com/example/mrt4you_mobile/Graph.java
@@ -6,11 +6,8 @@ import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.ProtocolException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
--- app/src/main/java/com/example/mrt4you_mobile/StartActivity.java
+++ app/src/main/java/com/example/mrt4you_mobile/StartActivity.java
@@ -6,8 +6,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.util.Log;
-import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
